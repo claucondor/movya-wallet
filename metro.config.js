e7ce2b1a12dc0f0294bb738f0fd3a7af0d79d0b7
Claudio Condor <claucondor@gmail.com>
2025-11-18 08:10:42 -0400

    feat: add crypto-browserify and polyfills for Node.js modules
    - Updated package.json to include crypto-browserify, readable-stream, and stream-browserify.
    - Modified polyfills.js to import and set global.crypto using crypto-browserify for compatibility with @stacks/* libraries.

--- metro.config.js
+++ metro.config.js
@@ -21,6 +21,8 @@ module.exports = (async () => {
         util: require.resolve('util/'),
         buffer: require.resolve('buffer/'),
         process: require.resolve('process/browser'),
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
       },
     },
   };
--- polyfills.js
+++ polyfills.js
@@ -1,10 +1,12 @@
 // Polyfills for Node.js modules required by @stacks/* libraries
 import { Buffer } from 'buffer';
 import process from 'process';
+import crypto from 'crypto-browserify';
 
 // Make Buffer and process available globally
 global.Buffer = Buffer;
 global.process = process;
+global.crypto = crypto;
 
 // Set NODE_ENV if not set
 if (!process.env.NODE_ENV) {
