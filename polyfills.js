b24b17014f69d8aa236bdf034bd8b8047a869c9c
Claudio Condor <claucondor@gmail.com>
2025-11-18 08:18:41 -0400

    feat: enhance crypto setup by configuring global.crypto.web and merging crypto-browserify functions

--- polyfills.js
+++ polyfills.js
@@ -1,4 +1,5 @@
 // Import react-native-get-random-values FIRST before anything else
+// This sets up global.crypto.getRandomValues automatically
 import 'react-native-get-random-values';
 
 // Polyfills for Node.js modules required by @stacks/* libraries
@@ -14,8 +15,22 @@ if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = __DEV__ ? 'development' : 'production';
 }
 
-// Import crypto after setting up Buffer and process
-const crypto = require('crypto-browserify');
-global.crypto = crypto;
+// After react-native-get-random-values is imported, global.crypto.getRandomValues should exist
+// Setup crypto.web for @stacks libraries that use crypto.web.getRandomValues
+if (!global.crypto.web) {
+  global.crypto.web = {
+    getRandomValues: global.crypto.getRandomValues,
+    subtle: global.crypto.subtle || {}
+  };
+}
+
+// Import crypto-browserify after setting up the web crypto API
+const cryptoBrowserify = require('crypto-browserify');
+// Merge crypto-browserify functions into global.crypto without overwriting web
+Object.keys(cryptoBrowserify).forEach(key => {
+  if (key !== 'web' && key !== 'getRandomValues') {
+    global.crypto[key] = cryptoBrowserify[key];
+  }
+});
 
 export {};
